64c0c799d89bbc70d7fc4ff90a477d88f961c27b
mboehl <matze@cuk24.de>
2026-06-25 16:19:46 +0200

    comments

--- main.py
+++ main.py
@@ -20,10 +20,15 @@ if __name__ == "__main__":
                 "data/periodic_distance_matrix.txt"]
 
     matrices = [np.loadtxt(file) for file in files]
+
+    # create labels for each matrix based on the file name
     labels = [file.split("/")[-1].split(".")[0].split("_")[0] for file in files]
+    
+    # specify whether each matrix is a recurrence matrix or not
+    is_rec_matrix = [False, False, False]  
 
     # create RPPlayer for each matrix
-    recurrence_players = [RPPlayer(matrix, label=label, is_rec_matrix=False) for matrix, label in zip(matrices, labels)]
+    recurrence_players = [RPPlayer(matrix, label=label, is_rec_matrix=is_rec) for matrix, label, is_rec in zip(matrices, labels, is_rec_matrix)]
 
     # create and show application window
     app = ApplicationWindow(recurrence_players)
